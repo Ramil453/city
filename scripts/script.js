de3bb7cbbf221f62908b7d9363a8027f4784e914
Ramil453 <126707489+Ramil453@users.noreply.github.com>
2024-03-05 22:13:21 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+document.querySelector('.facts-btn').onclick = function () {
+	document.querySelector('.facts').scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.info-btn').onclick = function () {
+	document.querySelector('.information').scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.attractions-btn').onclick = function () {
+	document.querySelector('.attractions').scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.review-btn').onclick = function () {
+	document
+		.querySelector('.city-overview')
+		.scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.main-btn').onclick = function () {
+	document.querySelector('.header').scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.facts-btn2').onclick = function () {
+	document.querySelector('.facts').scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.info-btn2').onclick = function () {
+	document.querySelector('.information').scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.review-btn2').onclick = function () {
+	document
+		.querySelector('.city-overview')
+		.scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.attractions-btn2').onclick = function () {
+	document.querySelector('.attractions').scrollIntoView({ behavior: 'smooth' })
+}
+document.querySelector('.things-btn').onclick = function () {
+	document.querySelector('.things').scrollIntoView({ behavior: 'smooth' })
+}
